0fb2ed7687a23ace8a62137edafbfbac8b6f30da
earthrobot <syley-20@yandex.ru>
2018-07-05 22:29:40 +0400

    правки после проверки проекта

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var button = document.querySelector(".search-hotel__button");
 var form = document.querySelector(".search-hotel__form");
+
 button.addEventListener("click", function (evt) {
-evt.preventDefault();
-form.classList.toggle("display-none");
+    evt.preventDefault();
+    form.classList.toggle("display-none");
 });
