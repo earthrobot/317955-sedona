fda6142d58e2d26610e5cb03e8747de36035fe26
earthrobot <syley-20@yandex.ru>
2018-07-07 12:52:55 +0400

    правки после проверки 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
+document.documentElement
+.classList.replace('no-js', 'js');
+
 var button = document.querySelector(".search-hotel__button");
 var form = document.querySelector(".search-hotel__form");
 
 button.addEventListener("click", function (evt) {
     evt.preventDefault();
     form.classList.toggle("display-none");
+    form.classList.toggle("form-animation");
 });
+
